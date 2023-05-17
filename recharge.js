98ed1f5293f022c67c373f6a7511cf721541b65b
Uvindu Rajapakshe <118661330+UvinduBro@users.noreply.github.com>
2023-05-17 06:25:26 +0000

    modified:   recharge.js 	new file:   tab. 	new file:   tab.html

--- recharge.js
+++ recharge.js
@@ -1,10 +1,9 @@
-const submitButton = document.getElementById("submitButton");
-submitButton.addEventListener("click", function(event) {
+const msubmitButton = document.getElementById("mobitelsubmitButton");
+msubmitButton.addEventListener("click", function(event) {
   event.preventDefault();
   
-  const accountNumber = document.getElementById("accountNumber").value;
-  const reloadCard = document.getElementById("reload-card").value;
-  const ussdCode = `*679*4*${accountNumber}*${reloadCard}%23`;
+  const mreloadCard = document.getElementById("mobitelreload-card").value;
+  const mussdCode = `*102*${mreloadCard}#`;
   
-  window.location.href = `tel:${ussdCode}`;
+  window.location.href = `tel:${mussdCode}`;
 });
